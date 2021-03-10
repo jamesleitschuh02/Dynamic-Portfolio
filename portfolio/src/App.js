ab1b54bd819e8fd57f82cbf54de8dc4a4287e3dc
James Leitschuh <james.leitschuh02@gmail.com>
2021-03-10 16:43:55 -0600

    added routes to App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,3 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import 
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <h3>HEADER</h3>
+                <Wrapper>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/home" component={Home} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                </Wrapper>
+                <h3>FOOTER</h3>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
