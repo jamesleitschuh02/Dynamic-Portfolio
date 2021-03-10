c439577b425b026a4c2a876ed60619724cc1820d
James Leitschuh <james.leitschuh02@gmail.com>
2021-03-10 17:59:45 -0600

    added images to utils

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -3,21 +3,23 @@ import Column from '../components/Column/Column';
 import Row from '../components/Row/Row';
 import Container from '../components/Container/Container';
 
+const profilePicture = require("../utils/Images/me.jpg");
+
 function Home() {
     return(
         <Container>
             <Row>
                 <Column size={4}>
-                    <img src="../public/Images/me.jpg" alt="me" className="responsive" /> 
+                    <img src={profilePicture} alt="me" className="responsive" /> 
                 </Column>               
                 <Column size={4}>
                     <h2>About Me</h2>
-                        <Row>
-                            <Column size={12}>
-                                <p>Hi, my name is James Leitschuh. I am currently enrolled in the U of M coding bootcamp. With a minimal background in coding, I'm training to aquire the skills to find a succesful career in coding.
-                                </p>
-                            </Column>
-                        </Row>
+                    <Row>
+                        <Column size={12}>
+                            <p>Hi, my name is James Leitschuh. I am currently enrolled in the U of M coding bootcamp. With a minimal background in coding, I'm training to aquire the skills to find a succesful career in coding.
+                            </p>
+                        </Column>
+                    </Row>
                 </Column>
             </Row>
         </Container>
